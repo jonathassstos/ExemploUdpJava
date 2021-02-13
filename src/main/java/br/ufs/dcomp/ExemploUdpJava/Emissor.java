b48b4cda6ee0d88c2ab28df0a6d22ea6d721bdf6
Ubuntu <ubuntu@ip-172-31-39-192.ec2.internal>
2021-02-13 16:02:14 +0000

    Commit com alterações/inserção de detalhes das partes do código

--- src/main/java/br/ufs/dcomp/ExemploUdpJava/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploUdpJava/Emissor.java
@@ -2,20 +2,21 @@ package br.ufs.dcomp.ExemploUdpJava;
 
 import java.net.*;
 
-public class AppUDP1 {
+public class Emissor {
 
     public static void main(String[] args) throws SocketException {
         try{
             System.out.print("[ Alocando porta UDP      ..................  ");
-    	    DatagramSocket socket = new DatagramSocket(10000);
+    	    DatagramSocket socket = new DatagramSocket(10000); // socket udp associado a porta 10000
             System.out.println("[OK] ]");
             
             String msg = "Olá!!!";
             
-            byte[] msg_buf = msg.getBytes();
-            int msg_size = msg_buf.length;
-            InetAddress destination_address = InetAddress.getLocalHost();
-            int destination_port = 20000; 
+            byte[] msg_buf = msg.getBytes(); // Pega a representação em bytes do que quer enviar
+            int msg_size = msg_buf.length; // pega o tamanho do vetor de bytes e armazena na var do tamanho dele
+            InetAddress destination_address = InetAddress.getLocalHost(); // como o receptor e emissor estão
+            // na mesma máquina o endereço de destino é colocado como o local
+            int destination_port = 20000; // identifica a porta de destino
 
             System.out.print("[ Montando datagrama UDP  ..................  ");
             DatagramPacket pack = new DatagramPacket(msg_buf, msg_size, destination_address, destination_port);
--- src/main/java/br/ufs/dcomp/ExemploUdpJava/Receptor.java
+++ src/main/java/br/ufs/dcomp/ExemploUdpJava/Receptor.java
@@ -2,26 +2,28 @@ package br.ufs.dcomp.ExemploUdpJava;
 
 import java.net.*;
 
-public class AppUDP2 {
+public class Receptor {
 
     public static void main(String[] args) throws SocketException {
         try{
 
             System.out.print("[ Alocando porta UDP                  ..................  ");
-    	    DatagramSocket socket = new DatagramSocket(20000);
+    	    DatagramSocket socket = new DatagramSocket(20000); // se associa a um socket na porta 20000
             System.out.println("[OK] ]");
 
             byte[] buf = new byte[20];
-            DatagramPacket pack = new DatagramPacket(buf, buf.length);
+            DatagramPacket pack = new DatagramPacket(buf, buf.length); // cria um pacote vazio 
+            // para poder receber a mensagem nesse pacote
 
             System.out.print("[ Aguardando recebimento de mensagem  ..................  ");
-            socket.receive(pack);
+            socket.receive(pack); // operação bloqueante
             System.out.println("[OK] ]");
             
-            byte[] received_data = pack.getData();
-            String received_msg = new String(received_data); 
-            InetAddress origin_address = pack.getAddress();
-            int origin_port = pack.getPort();
+            //preenchendo o pacote que estava vazio
+            byte[] received_data = pack.getData(); // pega os Dados em bytes
+            String received_msg = new String(received_data); // transforma os dados em string
+            InetAddress origin_address = pack.getAddress(); // pega o endereço de origem
+            int origin_port = pack.getPort(); // pega a porta de origem
             
             System.out.println("  Mensagem:             "+received_msg);
             System.out.println("  Endereço de origem:   "+origin_address.getHostAddress());
